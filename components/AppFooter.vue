f6ae20df5fa8b347502e22e9628206e008b128cc
VincentBergerault <VincentBergerault>
2023-10-18 01:18:21 +0200

    update

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,3 +1,21 @@
 <template>
-  <span> ptit texte en sah avec le lien vers les CGU et un disclamer </span>
+  <v-footer :padless="true">
+    <v-card
+      flat
+      tile
+      width="100%"
+      style="background-color: #e98499; text-align: center"
+    >
+      <br />
+      <v-card-text class="white--text pt-0">
+        This Website is currently a non profit website, it's only purpose is to
+        represent the art of Clemonart and to provide a proper gallery.
+        <br />
+        In a not so far future, this website will be able to provide prices for
+        the paintings, via Paypal by contacting the artist Clemonart.
+        <br />
+        Thank you for your understanding.
+      </v-card-text>
+    </v-card>
+  </v-footer>
 </template>
--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,4 +1,30 @@
 <template>
-  <span> application menu stay on top </span>
-  <span>about me <span> </span>gallery<span> </span> contact us </span>
+  <header>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/art">Art</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
+  <!--
+  <v-app-bar app color="#E98499">
+    <v-toolbar-title style="width: 350px">
+      <div style="float: left">
+        <v-img src="../static/images/logo.png" height="50px" width="50px" />
+      </div>
+      <a class="white--text" style="text-decoration: none" text-align="center"
+        >&nbsp;Clemonart</a
+      >
+    </v-toolbar-title>
+    <v-text-field
+      flat
+      solo-inverted
+      hide-details
+      prepend-inner-icon="mdi-magnify"
+      label="Search"
+      class="hidden-sm-and-down pl-10 ml-4"
+    />
+  </v-app-bar>-->
 </template>
