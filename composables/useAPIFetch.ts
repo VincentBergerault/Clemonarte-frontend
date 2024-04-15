34d13a19d380d93e03eb4ad65488ad77cfe6bc4f
VincentBergerault <bergerault.vb@gmail.com>
2024-04-15 23:31:53 +0200

    update custom fetch remove trailing slash function

--- composables/useAPIFetch.ts
+++ composables/useAPIFetch.ts
@@ -1,5 +1,15 @@
 export const useApiFetch = async (url: string, options: any = {}) => {
   const config = useRuntimeConfig();
   options.server = false;
-  return await useFetch(`${config.public.apiBase}/${url}`, options);
+  return await useFetch(
+    `${removeTrailingSlash(config.public.apiBase)}/${url}`,
+    options
+  );
 };
+
+function removeTrailingSlash(url: string): string {
+  if (url.endsWith("/")) {
+    return url.slice(0, -1);
+  }
+  return url;
+}
