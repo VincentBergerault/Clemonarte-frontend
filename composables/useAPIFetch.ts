727cbe3e5ea6810edfa0fb6703a6e795c07c57b9
VincentBergerault <bergerault.vb@gmail.com>
2024-04-15 23:27:35 +0200

    fix custom use fetch (any type need to change)

--- composables/useAPIFetch.ts
+++ composables/useAPIFetch.ts
@@ -1,4 +1,5 @@
-export const useApiFetch = async (url: string, options = {}) => {
+export const useApiFetch = async (url: string, options: any = {}) => {
   const config = useRuntimeConfig();
+  options.server = false;
   return await useFetch(`${config.public.apiBase}/${url}`, options);
 };
